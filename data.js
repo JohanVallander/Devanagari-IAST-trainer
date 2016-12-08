9c0a73da457af7831bd856ebc7ed9853c89268e9
johan wallander <johan@johan.com>
2016-12-08 23:56:39 +0100

    initial commit

---
+++ data.js
@@ -0,0 +1,59 @@
+var verseData = {
+    delimiter:"।", // means we insert a newline upon this character,
+    unMapped:[" ","।","॥"],//these characters should not be part of the "game"
+    verses:
+    [
+	{ 
+	    description:"Introductory Doha 1",
+	    sanskrit: "श्रीगुरु चरन सरोज रज निज मन मुकुरु सुधारि । बरनउँ रघुबर बिमल जसु जो दायकु फल चारि॥",
+	    mappings: [
+		["श्री","śrī"],
+		["गु","gu"],
+		["रु","ru"],
+		["च","ca"],
+		["र","ra"],
+		["न","na"],
+		["स","sa"],
+		["रो","ro"],
+		["ज","ja"],
+		["र","ra"],
+		["ज","ja"],
+		["नि","ni"],
+		["ज","ja"],
+		["म","ma"],
+		["न","na"],
+		["मु","mu"],
+		["कु","ku"],
+		["रु","ru"],
+		["सु","śu"],
+		["धा","dhā"],
+		["रि","ri"],
+		["ब","ba"],
+		["र","ra"],
+		["न","na"],
+		["उँ","u"],
+		["र","ra"],
+		["घु","ghu"],
+		["ब","ba"],
+		["र","ra"],
+		["बि","bi"],
+		["म","ma"],
+		["ल","la"],
+		["ज","ja"],
+		["सु","śu"],
+		["जो","jo"],
+		["दा","dā"],
+		["य","ya"],
+		["कु","ku"],
+		["फ","pha"],
+		["ल","la"],
+		["चा","cā"],
+		["रि","ri"]
+	    ]
+	    //shrīguru charana saroja raja nija mana mukuru sudhāri।
+	    //baranau raghubara bimala jasu jo dāyaku phala chāri॥
+	}
+	//next verse to come here... eventually
+    ]
+};
+
---
+++ deva.js
@@ -0,0 +1,84 @@
+
+var deva={
+    updateSanskrit: function(){
+	ready = '<span class="text-success">'+deva.readySanskrit+'</span>';
+	remaining = '<span class="text-muted">'+deva.remainingSanskrit+'</span>';
+	sanskrit = ready + remaining;
+	sanskrit = sanskrit.replace(verseData.delimiter,verseData.delimiter+"<br/>");
+	$("#sanskrit_verse").html(sanskrit);	
+    },
+    loadVerse: function(verseNr){
+	deva.currentVerse=verseData.verses[verseNr]
+	
+	deva.readySanskrit="";
+	deva.remainingSanskrit=deva.currentVerse.sanskrit;
+	deva.updateSanskrit();
+	deva.mappings = deva.scrambleList(this.currentVerse.mappings,100);
+	$("#syllables").empty();
+	for(i in this.mappings){
+	    devanagari = deva.mappings[i][0];
+	    iast = deva.mappings[i][1];
+
+	    button = $('<button type="button" class="syllable-button btn btn-primary" value="' + devanagari + '">' + iast +'</button>');
+	    $("#syllables").append(button);
+	}
+	$("button.syllable-button").click(function(){
+	    deva.syllableClick(this);
+	});	
+    },
+    
+    syllableClick : function(button){
+	var devanagari = $(button).attr("value");
+	console.log("click " + devanagari);
+
+	var strippedRemaining = deva.remainingSanskrit;
+	
+	for(var i in verseData.unMapped){
+	    var unMapped=verseData.unMapped[i];
+	    //not so pretty but couldn't find out how to make a global replace without regexp
+	    while(strippedRemaining.search(unMapped) != -1){
+		strippedRemaining=strippedRemaining.replace(unMapped,"");
+	
+	    }
+	    
+	
+	}
+	console.log("=========================");
+	console.log(deva.readySanskrit);
+	console.log(deva.remainingSanskrit);
+	console.log(strippedRemaining);
+	if(strippedRemaining.search(devanagari) == 0){
+	    //right button, we should make some more letters green
+	    console.log("hit");
+	    
+	    var index = deva.remainingSanskrit.search(devanagari) + devanagari.length;
+
+	    deva.readySanskrit += deva.remainingSanskrit.slice(0,index);
+	    deva.remainingSanskrit = deva.remainingSanskrit.slice(index);
+	    deva.updateSanskrit();
+	}
+    },
+    scrambleList: function(list,maxOutOfOrder){
+	var newList=[];
+	for(i in list){
+	    var item=list[i].slice();
+	    newList.push(item);//We want a deep copy
+	    
+	}
+	var returnList=[];
+	while(newList.length){
+	    var rightEnd=Math.min(newList.length-1,maxOutOfOrder);
+	    var index=Math.floor(Math.random()*rightEnd);
+	    var item=newList.splice(index,1)[0];
+	    returnList.push(item);
+	}
+	return returnList;
+    }
+    
+
+    
+}
+
+$(function(){
+    deva.loadVerse(0);
+});
