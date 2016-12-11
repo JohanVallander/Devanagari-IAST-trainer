a589eec655dc11d7c8cb779c6c803e19d99e28b1
johan wallander <johan@johan.com>
2016-12-11 21:24:10 +0100

    some refactoring and updates was done. fixed bad logic. Changed so only 7 syllables are visible at a time

--- deva.js
+++ deva.js
@@ -1,64 +1,83 @@
 
 var deva={
-    updateSanskrit: function(){
-	ready = '<span class="text-success">'+deva.readySanskrit+'</span>';
-	remaining = '<span class="text-muted">'+deva.remainingSanskrit+'</span>';
-	sanskrit = ready + remaining;
-	sanskrit = sanskrit.replace(verseData.delimiter,verseData.delimiter+"<br/>");
-	$("#sanskrit_verse").html(sanskrit);	
-    },
-    loadVerse: function(verseNr){
-	deva.currentVerse=verseData.verses[verseNr]
-	
-	deva.readySanskrit="";
-	deva.remainingSanskrit=deva.currentVerse.sanskrit;
-	deva.updateSanskrit();
-	deva.mappings = deva.scrambleList(this.currentVerse.mappings,100);
+    maxVisibleSyllables : 7,  //Nr of clickable IAST choices visible to the user
+    initSyllables : function(mappings){
+	this.mappings = this.scrambleList(mappings,this.maxVisibleSyllables);
 	$("#syllables").empty();
-	for(i in this.mappings){
-	    devanagari = deva.mappings[i][0];
-	    iast = deva.mappings[i][1];
-
-	    button = $('<button type="button" class="syllable-button btn btn-primary" value="' + devanagari + '">' + iast +'</button>');
-	    $("#syllables").append(button);
+	for(var i=0;i<this.maxVisibleSyllables; i++){
+	    this.addSyllableButton();
 	}
-	$("button.syllable-button").click(function(){
+    },
+    addSyllableButton : function(){
+	m = this.mappings.shift();
+	devanagari = m[0];
+	iast = m[1];    
+	button = $('<button type="button" class="syllable-button btn btn-primary" value="' + devanagari + '">' + iast +'</button>');
+	button.hide();
+	$("#syllables").append(button);
+	button.click(function(){
 	    deva.syllableClick(this);
-	});	
+	});
+	button.fadeIn(1000);	   
+    },
+    scrambleList: function(list,maxOutOfOrder){
+	var scrambledList=[];
+	for(i in list){
+	    var item={
+		element:list[i].slice(),
+		originalPosition:i
+	    };
+	    scrambledList.push(item);
+	    
+	}
+	for(i=0;i<1000;i++){
+	    //kind of a randomized bubble sort that before every swap checks that they don't get to much out of order
+	    //This is because we only display a limited number of choices for the user,
+	    //and the required choice has to be within this limited set.
+	    var swapPos1=Math.floor(Math.random()*scrambledList.length);
+	    var swapPos2=Math.floor(Math.random()*scrambledList.length);
+	    var swap1 = scrambledList[swapPos1];
+	    var swap2 = scrambledList[swapPos2];
+	    
+	    if(swapPos2-swap1.originalPosition >= maxOutOfOrder)
+		continue;
+	    if(swapPos1-swap2.originalPosition >= maxOutOfOrder)
+		continue;
+	    
+	    scrambledList[swapPos1]=swap2;
+	    scrambledList[swapPos2]=swap1;	
+	}
+	returnList=[];
+	for(var i in scrambledList){
+	    returnList.push(scrambledList[i].element);
+	}
+	return returnList;
     },
     
     syllableClick : function(button){
 	var devanagari = $(button).attr("value");
-	console.log("click " + devanagari);
-
-	var strippedRemaining = deva.remainingSanskrit;
-	
-	for(var i in verseData.unMapped){
-	    var unMapped=verseData.unMapped[i];
-	    //not so pretty but couldn't find out how to make a global replace without regexp
-	    while(strippedRemaining.search(unMapped) != -1){
-		strippedRemaining=strippedRemaining.replace(unMapped,"");
-	
-	    }
-	    
-	
-	}
-	if(strippedRemaining.search(devanagari) == 0){
+	if(devanagari == this.currentVerse.mappings[this.mappingsPosition][0]){
 	    //correct button clicked , we should make some more letters green
-	    console.log("hit");
+	    this.mappingsPosition++;
+	    var index = this.remainingSanskrit.search(devanagari) + devanagari.length;
 	    
-	    var index = deva.remainingSanskrit.search(devanagari) + devanagari.length;
-
-	    deva.readySanskrit += deva.remainingSanskrit.slice(0,index);
-	    deva.remainingSanskrit = deva.remainingSanskrit.slice(index);
-
-	    deva.updateSanskrit();
+	    this.readySanskrit += deva.remainingSanskrit.slice(0,index);
+	    this.remainingSanskrit = deva.remainingSanskrit.slice(index);
+	    
+	    this.updateSanskrit();
 	    //and we should turn the button green
 	    $(button).removeClass('btn-primary');
 	    $(button).addClass('btn-success');
-  
-	    $(button).delay(337).fadeOut(1000);
-	    
+	    //$(button).click(function(){})//do nothing if the user clicks twice 
+	    $(button).fadeOut(1000);
+	    if(this.mappings.length>0){
+		setTimeout(function () {
+		    deva.addSyllableButton();
+		},1000);
+	    }else{
+		alert("congratz");
+		location.reload(); //something more fancy should happen, like going to next verse
+	    }
 	}else{
 	    $(button).removeClass('btn-primary');
 	    $(button).addClass('btn-danger');
@@ -68,25 +87,23 @@ var deva={
 	    },1000);
 	}
     },
-
-    scrambleList: function(list,maxOutOfOrder){
-	var newList=[];
-	for(i in list){
-	    var item=list[i].slice();
-	    newList.push(item);//We want a deep copy
-	    
-	}
-	var returnList=[];
-	while(newList.length){
-	    var rightEnd=Math.min(newList.length-1,maxOutOfOrder);
-	    var index=Math.floor(Math.random()*rightEnd);
-	    var item=newList.splice(index,1)[0];
-	    returnList.push(item);
-	}
-	return returnList;
-    }
     
-
+    updateSanskrit: function(){
+	ready = '<span class="text-success">'+deva.readySanskrit+'</span>';
+	remaining = '<span class="text-muted">'+deva.remainingSanskrit+'</span>';
+	sanskrit = ready + remaining;
+	sanskrit = sanskrit.replace(verseData.delimiter,verseData.delimiter+"<br/>");
+	$("#sanskrit_verse").html(sanskrit);	
+    },
+    
+    loadVerse: function(verseNr){
+	this.currentVerse=verseData.verses[verseNr]
+	this.readySanskrit="";
+	this.remainingSanskrit=this.currentVerse.sanskrit;
+	this.updateSanskrit();
+	this.initSyllables(this.currentVerse.mappings);
+	this.mappingsPosition=0;
+    },
     
 }
 
